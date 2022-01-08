900cda12887cc28a4519dda7895b7866ef9ff8a0
Roland Lauterbach <info@rolix.de>
2022-01-08 11:41:24 +0100

    synchronisiert mit pool01

--- views/live/help.php
+++
@@ -1,8 +0,0 @@
-  <p class="lead">Das Modul live liefert einen JS-Player aus, den man in einen iFrame einbauen kann.</p>
-  <h5>URL</h5>
-  <code><span class="text-success"><?php echo 'https://'.$_SERVER['SERVER_NAME']._URL_STUB_;?>/player/</span><abbr title="Eine Methode des Moduls"><b><em>meth</em></b></abbr>/<abbr title="Shorttag der Kamera"><b><em>st</em></b></abbr>/<abbr title="Parameter der Methode"><b><em>par</em></b></abbr></code>
-  <hr><h5>Methoden</h5>
-  <div class="row">
-    <div class="col-sm-1"><a href="<?php echo _URL_STUB_.'/live/player/help';?>">player</a></div>
-    <div class="col-sm-8"><p>Live-Bild einer Kamera mit Player.</p></div>
-  </div>
